ebeb3d41e95206a980f659f3a0bf54030fe74a77
Minh Ma <minh.ma@capgemini-gs.com>
2022-04-24 10:11:06 -0700

    commit

--- .sfdx/typings/lwc/apex/cloneCampaignController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/cloneCampaignController.getCampaignsList" {
-  export default function getCampaignsList(param: {pagenumber: any, numberOfRecords: any, pageSize: any, searchString: any}): Promise<any>;
-}
-declare module "@salesforce/apex/cloneCampaignController.getCampaignsCount" {
-  export default function getCampaignsCount(param: {searchString: any}): Promise<any>;
-}
-declare module "@salesforce/apex/cloneCampaignController.cloneSelectedCampaigns" {
-  export default function cloneSelectedCampaigns(param: {selectedCampaigns: any}): Promise<any>;
-}
